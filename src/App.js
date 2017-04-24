57297cf6122f493d9bf9bf1c72256f674cb55566
Michael Nagy <michaelnt8@gmail.com>
2017-04-24 14:00:51 -0300

    Footer
    footer component finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import MainMenu from './Components/MainMenu/MainMenu';
 import Headline from './Components/Headline/Headline';
 import GithubRepo from './Components/GithubRepo/GithubRepo';
 import Features from './Components/Features/Features';
+import { Container, Icon } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MainMenu/>
+        <GithubRepo/>
         <Headline/>
         <Features/>
-        <GithubRepo/>
+        <Container fluid={true} textAlign={'center'} style={{padding:'0.5em', backgroundColor: '#E8E8E8', fontSize:'0.9em', color:'rgba(0,0,0,.4)'}}>
+          <p>made with <Icon name='heart' color='pink' size='large'/> by <a style={{color:'#77828e'}} href='https://github.com/michaelnagy' target='_blank'>Michael Nagy</a> </p>
+        </Container>
       </div>
     );
   }
